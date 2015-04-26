91e13ae4d456e524011fd033d6025c657f3fccd5
Alessio Santo <alessio.santocs@gmail.com>
2015-04-26 21:03:38 +0200

    Not delivering on create

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -3,7 +3,7 @@ class Notification < ActiveRecord::Base
   has_many :user_notifications
   has_many :users, through: :user_notifications
 
-  after_create :deliver
+  # after_create :deliver
 
   def deliver
     users = User.notifiable
